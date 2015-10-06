bff3551344a35c4206126c3896bf25f394bba42b
Jermyn Lai <rhemus_lupin@hotmail.com>
2015-10-06 17:03:59 +0800

    Parse JSON Array
    Changed the createItem method to return JSON Array instead.

--- Chronos/src/application/Parser.java
+++ Chronos/src/application/Parser.java
@@ -1,23 +1,31 @@
 package application;
 
+import java.lang.String;
+
+import org.json.simple.JSONArray;
+
 public class Parser {
 	
-	public Parser() {}
+	private JSONArray taskArr;
 	
-	public String[] createItem(String content) {
-		//int numContents;
+	public Parser() {
+		taskArr = new JSONArray();
+	}
+	
+	public JSONArray createItem(String content) {
+		int numContents;
 		String[] contents = content.split(", ");
 		
-		/*for(numContents=0; numContents<contents.length; numContents++) {
-			contents[numContents]=contents[numContents].trim();
-			if((contents[numContents].startsWith("p"))) {
+		for(numContents=0; numContents<contents.length; numContents++) {
+			taskArr.add(contents[numContents]);
+			/*if((contents[numContents].startsWith("p"))) {
 				contents[numContents] = contents[numContents].replaceFirst("p:", "");
 			}
 			else if((contents[numContents].startsWith("c"))) {
 				contents[numContents] = contents[numContents].replaceFirst("c:", "");
-			}
-		}*/
-		return contents;
+			}*/
+		}
+		return taskArr;
 	}
 	
 	public String getID(String contentString) {
@@ -25,7 +33,6 @@ public class Parser {
 		String itemID = contents[0];
 		return itemID;
 	}
-	
 	
 }
 
