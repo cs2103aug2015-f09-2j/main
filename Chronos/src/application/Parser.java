12b7b52dbbcba9c92a15caba1ca5f35459fb7e5c
Jermyn Lai <rhemus_lupin@hotmail.com>
2015-10-07 16:46:36 +0800

    Convert To JSONArray

--- Chronos/src/application/Parser.java
+++ Chronos/src/application/Parser.java
@@ -1,37 +1,62 @@
 package application;
 
+import gui.Item;
+
 import java.lang.String;
 
 import org.json.simple.JSONArray;
 
+
+
 public class Parser {
 	
 	private JSONArray taskArr;
+	private Item item;
+	private Integer id;
+	private String taskID;
 	
 	public Parser() {
 		taskArr = new JSONArray();
+		id = 0;
+		taskID = "t";
 	}
 	
-	public JSONArray createItem(String content) {
-		int numContents;
+	public Item createItem(String content) {
+		//int numContents;
+		id++;
 		String[] contents = content.split(", ");
 		
-		for(numContents=0; numContents<contents.length; numContents++) {
-			taskArr.add(contents[numContents]);
-			/*if((contents[numContents].startsWith("p"))) {
+		/*for(numContents=0; numContents<contents.length; numContents++) {
+			convertToJsonArray(contents[numContents]);
+			if((contents[numContents].startsWith("p"))) {
 				contents[numContents] = contents[numContents].replaceFirst("p:", "");
 			}
 			else if((contents[numContents].startsWith("c"))) {
 				contents[numContents] = contents[numContents].replaceFirst("c:", "");
-			}*/
+			}
+		}*/
+		if(!contents[4].isEmpty()) {
+			item = new Item(taskID.concat(id.toString()), contents[2].toString(), contents[0].toString(), contents[4].toString());
+		} else {
+			item = new Item(taskID.concat(id.toString()), contents[2].toString(), contents[0].toString(), "");
 		}
-		return taskArr;
+		
+		return item;
 	}
 	
 	public String getID(String contentString) {
 		String[] contents = contentString.split(", ");
 		String itemID = contents[0];
 		return itemID;
+	}
+	
+	public JSONArray convertToJsonArray(String[] content) {
+		int numContents;
+		
+		for(numContents=0; numContents<content.length; numContents++) {
+			taskArr.add(content[numContents]);
+		}
+		return taskArr;
 	}
 	
 }
