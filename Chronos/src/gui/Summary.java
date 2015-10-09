08480e9d076a7ee73eaacb16c3390cfd537ba88a
lixiang <lixiangabcd08@hotmail.com>
2015-10-10 01:31:50 +0800

    added in notes column, column width resized

--- Chronos/src/gui/Summary.java
+++ Chronos/src/gui/Summary.java
@@ -6,11 +6,14 @@ import application.Task;
 import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.control.TableCell;
 import javafx.scene.control.TableColumn;
 //import javafx.scene.control.ListView;
 import javafx.scene.control.TableView;
 import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.scene.layout.StackPane;
+import javafx.scene.paint.Color;
+import javafx.util.Callback;
 
 public class Summary extends StackPane {
 
@@ -52,6 +55,24 @@ public class Summary extends StackPane {
 		timeCol.setCellValueFactory(new PropertyValueFactory<Task, String>("endDate"));
 		titleCol.setCellValueFactory(new PropertyValueFactory<Task, String>("description"));
 		categoryCol.setCellValueFactory(new PropertyValueFactory<Task, String>("category"));
+		IDCol.setCellFactory(new Callback<TableColumn<Task, String>, TableCell<Task, String>>() {
+			public TableCell<Task, String> call(TableColumn<Task, String> param) {
+				return new TableCell<Task, String>() {
+
+					@Override
+					public void updateItem(String item, boolean empty) {
+						super.updateItem(item, empty);
+						if (!isEmpty()) {
+							this.setTextFill(Color.RED);
+							// Get fancy and change color based on data
+							if (item.contains("t"))
+								this.setTextFill(Color.BLUEVIOLET);
+							setText(item);
+						}
+					}
+				};
+			}
+		});
 	}
 
 }
