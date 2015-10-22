12831ebf2346f62e173712986705a521f29769c8
Xen Santos <a0126223@u.nus.edu>
2015-10-22 22:03:51 +0800

    Improved and refactored DoneCommand

--- Chronos/src/application/DoneCommand.java
+++ Chronos/src/application/DoneCommand.java
@@ -1,8 +1,6 @@
 package application;
 import application.Command;
 import application.Feedback;
-import application.Parser;
-import application.Storage;
 import application.Task;
 
 public class DoneCommand extends Command {
@@ -12,9 +10,12 @@ public class DoneCommand extends Command {
 
 	//Constant Strings
 	private static final String FEEDBACK_MESSAGE =  "Marked %1$s as done.";
+	private static final String FEEDBACK_COMPLETE = "Task %1$s is already complete.";
+	private static final String ERROR_INVALID_ID = "Error: ID doesn't exist.";
 	
 	private static final String LOG_MESSAGE = "Task %1$s marked as done.";
 	
+	protected static final String ID_EMPTY = "";
 	protected static final String UPDATE_STRING = ", s:";
 	
 	public DoneCommand(String content) {
@@ -25,16 +26,20 @@ public class DoneCommand extends Command {
 	public Feedback execute() {
 		String feedbackString = null;
 		Feedback feedback;
-		if(_content!="") {
+		if (_content != ID_EMPTY && _parse.isExistingId(_content, _store.entries_)) {
 			feedbackString = String.format(FEEDBACK_MESSAGE, _content);
 			_completedTask = _parse.retrieveTask(_content, _store.entries_);
-			_completedTask.markTaskAsDone(true);
-			_store.storeTemp();
-			String content = _completedTask.getId()+ UPDATE_STRING + _completedTask.isTaskComplete();
-			feedback = new UpdateCommand(content).execute();
-			feedback.setMessage(feedbackString);
-			_store.storeChanges();
-			log.info(String.format(LOG_MESSAGE, _content));
+			if (_completedTask.isTaskComplete()) {
+				feedback = new Feedback(String.format(FEEDBACK_COMPLETE, _content));
+			} else {
+				_completedTask.markTaskAsDone(true);
+				String content = _completedTask.getId() + UPDATE_STRING + _completedTask.isTaskComplete();
+				feedback = new UpdateCommand(content).execute(); //_store saving handled by UpdateCommand
+				feedback.setMessage(feedbackString);
+				log.info(String.format(LOG_MESSAGE, _content));
+			}
+		} else if (!_parse.isExistingId(_content, _store.entries_)) {
+			feedback = new Feedback(ERROR_INVALID_ID);
 		} else {
 			log.warning(LOG_NO_ID);
 			feedbackString = ERROR_NO_CONTENT;
@@ -46,10 +51,8 @@ public class DoneCommand extends Command {
 	@Override
 	public Feedback undo() {
 		_completedTask.markTaskAsDone(false); 
-		_store.storeTemp();
 		String content = _completedTask.getId()+ UPDATE_STRING + _completedTask.isTaskComplete();
 		Feedback feedback = new UpdateCommand(content).execute();
-		_store.storeChanges();
 		return feedback;
 	}
 	
