8d86b3aabe5068f0ec1cd5589db8de848c80a064
Jermyn Lai <rhemus_lupin@hotmail.com>
2015-10-28 23:29:11 +0800

    Refactoring classes

--- Chronos/src/application/Event.java
+++ Chronos/src/application/Event.java
@@ -8,16 +8,22 @@ public class Event extends Task {
 	
 	private String _startDate;
 	
+	private static final String DATE_SEPARATOR = " to ";
+	private static final String PRIORITY_HEADER = "p:";
+	private static final String CATEGORY_HEADER = "c:";
+	private static final String EMPTY_SPACE = " ";
+	private static final String ID_DISPLAY = ". ";
+	
 	public Event(String[] contents) throws ParseException {
 		super();
 		_description = contents[0];
 		for (int i = 1; i < contents.length; i++) {
-			if (contents[i].contains("p:")) {
+			if (contents[i].contains(PRIORITY_HEADER)) {
 				_priority = contents[i].substring(2).toLowerCase();
-			} else if (contents[i].contains("c:")) {
+			} else if (contents[i].contains(CATEGORY_HEADER)) {
 				_category = contents[i].substring(2);
 			} else { //date manipulation
-				String[] dates = contents[i].split(" to ");
+				String[] dates = contents[i].split(DATE_SEPARATOR);
 				_startDate = manipulateDate(dates[0]);
 				_endDate = manipulateDate(dates[1]);
 			}
@@ -39,7 +45,7 @@ public class Event extends Task {
 	
 	@Override
 	public String toString(){
-		return  _id + ". " + _description + " " +_startDate+" "+ _endDate + " " + _priority + " " + _category;
+		return  _id + ID_DISPLAY + _description + EMPTY_SPACE + _startDate+ EMPTY_SPACE + _endDate + EMPTY_SPACE + _priority + EMPTY_SPACE + _category;
 	}
 	
 	public String getStartDate() {
